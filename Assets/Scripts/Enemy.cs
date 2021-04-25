ca1e389209a8c156dcd36e8df9fe5970da734c63
Carlisle Hughes <22939626+carlislehughes@users.noreply.github.com>
2021-04-25 14:17:50 -0700

    Lesson D Done

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : MonoBehaviour
     GameObject player;
 
     public float speed = 4;
+
+    private float yBounds = -15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,10 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < yBounds)
+        {
+            Destroy(gameObject);
+        }
         Vector3 seekDirection = (player.transform.position - transform.position).normalized;
         rbEnemy.AddForce(seekDirection * speed * Time.deltaTime);
     }
